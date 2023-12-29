dad5230a84cbeb8ce22acc7e7c62d2bf95a605bc
Rafiul007 <rafiul.faisal@gmail.com>
2023-12-30 00:13:14 +0600

    signup page added

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,9 +1,21 @@
+import { TextField,Button } from '@mui/material'
 import React from 'react'
-
+import './Signup.css'
 function Signup() {
   return (
     <div className="signup-container">
-      
+      <div className="signup-heading">
+        <h1>Registration</h1>
+        <p>Join most secure and fastest deliery service in Bangladesh</p>
+      </div>
+
+      <form className='form-container'>
+        <TextField id="outlined-basic" label="Full Name" variant="outlined" fullWidth />
+        <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth />
+        <TextField id="outlined-basic" label="Password" variant="outlined" type='password' fullWidth />
+        <TextField id="outlined-basic" label="Confirm Password" variant="outlined" type='password' fullWidth />
+        <Button variant="contained">Registration</Button>
+      </form>
     </div>
   )
 }

38473190fe7fcf8357c7a00a5dae4a8144bfdd3f
Rafiul007 <rafiul.faisal@gmail.com>
2023-12-29 23:19:48 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
-import Home from './pages/Home';
-
+import "./App.css";
+import Home from "./pages/home/Home";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

66f520b5c85866606f10fab5c05bc4ade6262a90
Rafiul007 <rafiul.faisal@gmail.com>
2024-01-02 01:38:24 +0600

    invoice will be added

--- src/pages/createOrder/CreateORder.jsx
+++ src/pages/createOrder/CreateORder.jsx
@@ -15,97 +15,107 @@ function CreateORder() {
     ]
     return (
         <div className="create-order-container">
-            <div className="left-side">
-                <h1>Create Order</h1>
-                <div className="form-section">
-                    <form className='form-input-container'>
-                        <h2>Customer information</h2>
-                        <TextField
-                            fullWidth
-                            id="username"
-                            name="username"
-                            label="Username"
-                            type="username"
-                        //   value={formik.values.password}
-                        //   onChange={formik.handleChange}
-                        //   onBlur={formik.handleBlur}
-                        //   error={formik.touched.password && Boolean(formik.errors.password)}
-                        //   helperText={formik.touched.password && formik.errors.password}
-                        />
-                        <h2>Delivery information</h2>
-                        <div className="delivery-info-container">
-                            <div className='invoice-fields'>
-                                <TextField
-                                    fullWidth
-                                    id="invoice"
-                                    name="invoice"
-                                    label="Invoice ID (optional)"
-                                    type="text"
-                                //   value={formik.values.password}
-                                //   onChange={formik.handleChange}
-                                //   onBlur={formik.handleBlur}
-                                //   error={formik.touched.password && Boolean(formik.errors.password)}
-                                //   helperText={formik.touched.password && formik.errors.password}
-                                />
-                                <TextField
-                                    fullWidth
-                                    id="weight"
-                                    name="weight"
-                                    label="Weight (gm)"
-                                    type="number"
-                                //   value={formik.values.password}
-                                //   onChange={formik.handleChange}
-                                //   onBlur={formik.handleBlur}
-                                //   error={formik.touched.password && Boolean(formik.errors.password)}
-                                //   helperText={formik.touched.password && formik.errors.password}
-                                />
-                            </div>
-                            <div className='cash-fields'>
-                                <TextField
-                                    fullWidth
-                                    id="cash-collection"
-                                    name="cash-collection"
-                                    label="Cash Collection Amount"
-                                    type="number"
-                                //   value={formik.values.password}
-                                //   onChange={formik.handleChange}
-                                //   onBlur={formik.handleBlur}
-                                //   error={formik.touched.password && Boolean(formik.errors.password)}
-                                //   helperText={formik.touched.password && formik.errors.password}
-                                />
-                                <TextField
-                                    fullWidth
-                                    id="selling-price"
-                                    name="selling-price"
-                                    label="Selling price of the product"
-                                    type="number"
-                                //   value={formik.values.password}
-                                //   onChange={formik.handleChange}
-                                //   onBlur={formik.handleBlur}
-                                //   error={formik.touched.password && Boolean(formik.errors.password)}
-                                //   helperText={formik.touched.password && formik.errors.password}
-                                />
-                            </div>
-                            <div className="category-fields">
-                                <Autocomplete
-                                    disablePortal
-                                    id="combo-box-demo"
-                                    fullWidth
-                                    options={category}
-                                    renderInput={(params) => <TextField {...params} label="Category" />}
-                                />
-                            </div>
-                        </div>
-                        <div className="btn-cont">
-                        <Button color="primary" variant="contained" type="submit">Create</Button>
-                        <Button color="error" variant="outlined" type="submit"><Link to='/dashboard'>Cancel</Link></Button>
-                        </div>
-                    </form>
+            <div className="pack-cont">
+                <div className="packaging">
+                    <h1>Packaging convenience!</h1>
+                    <p>Worried about the packaging of your parcel? Don't worry, the right packaging solution is now available at RedX. Order now...</p>
+                    <Button variant="contained" color="success">Order now</Button>
                 </div>
             </div>
-            <div className="right-side">
-                <Invoice />
+            <div className="form-cont">
+                <div className="left-side">
+                    <h1>Create Order</h1>
+                    <div className="form-section">
+                        <form className='form-input-container'>
+                            <h2>Customer information</h2>
+                            <TextField
+                                fullWidth
+                                id="username"
+                                name="username"
+                                label="Username"
+                                type="username"
+                            //   value={formik.values.password}
+                            //   onChange={formik.handleChange}
+                            //   onBlur={formik.handleBlur}
+                            //   error={formik.touched.password && Boolean(formik.errors.password)}
+                            //   helperText={formik.touched.password && formik.errors.password}
+                            />
+                            <h2>Delivery information</h2>
+                            <div className="delivery-info-container">
+                                <div className='invoice-fields'>
+                                    <TextField
+                                        fullWidth
+                                        id="invoice"
+                                        name="invoice"
+                                        label="Invoice ID (optional)"
+                                        type="text"
+                                    //   value={formik.values.password}
+                                    //   onChange={formik.handleChange}
+                                    //   onBlur={formik.handleBlur}
+                                    //   error={formik.touched.password && Boolean(formik.errors.password)}
+                                    //   helperText={formik.touched.password && formik.errors.password}
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        id="weight"
+                                        name="weight"
+                                        label="Weight (gm)"
+                                        type="number"
+                                    //   value={formik.values.password}
+                                    //   onChange={formik.handleChange}
+                                    //   onBlur={formik.handleBlur}
+                                    //   error={formik.touched.password && Boolean(formik.errors.password)}
+                                    //   helperText={formik.touched.password && formik.errors.password}
+                                    />
+                                </div>
+                                <div className='cash-fields'>
+                                    <TextField
+                                        fullWidth
+                                        id="cash-collection"
+                                        name="cash-collection"
+                                        label="Cash Collection Amount"
+                                        type="number"
+                                    //   value={formik.values.password}
+                                    //   onChange={formik.handleChange}
+                                    //   onBlur={formik.handleBlur}
+                                    //   error={formik.touched.password && Boolean(formik.errors.password)}
+                                    //   helperText={formik.touched.password && formik.errors.password}
+                                    />
+                                    <TextField
+                                        fullWidth
+                                        id="selling-price"
+                                        name="selling-price"
+                                        label="Selling price of the product"
+                                        type="number"
+                                    //   value={formik.values.password}
+                                    //   onChange={formik.handleChange}
+                                    //   onBlur={formik.handleBlur}
+                                    //   error={formik.touched.password && Boolean(formik.errors.password)}
+                                    //   helperText={formik.touched.password && formik.errors.password}
+                                    />
+                                </div>
+                                <div className="category-fields">
+                                    <Autocomplete
+                                        disablePortal
+                                        id="combo-box-demo"
+                                        fullWidth
+                                        options={category}
+                                        renderInput={(params) => <TextField {...params} label="Category" />}
+                                    />
+                                </div>
+                            </div>
+                            <div className="btn-cont">
+                                <Button color="primary" variant="contained" type="submit">Create</Button>
+                                <Button color="error" variant="outlined" type="submit"><Link to='/dashboard'>Cancel</Link></Button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="right-side">
+                    <Invoice />
+                </div>
             </div>
+
 
 
         </div>

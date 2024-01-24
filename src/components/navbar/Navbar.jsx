7af0be5150ce105fea76ca0c2ac3e1862a3184ac
Rafiul007 <rafiul.faisal@gmail.com>
2024-01-25 00:29:46 +0600

    navbar resonsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import { Button, TextField } from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
-import './Navbar.css'
+import './Navbar.css';
+
 function Navbar() {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
+  useEffect(() => {
     const storedToken = localStorage.getItem('token');
-    return (
-        <div className="navbar-container">
-            <div className="logo-container">
-                <Link to='/'>
-                    <h1>Zipifly</h1>
-                </Link>
-            </div>
-            <div className="menu-container">
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/service'>Services</Link></li>
-                    <li><Link to='/offers'>Offers</Link></li>
-                </ul>
-            </div>
-            <div className="button-container">
-                <Button variant="outlined" color='success'><Link to='/welcome'>{
-                    storedToken ? `Dashboard` : `Log in`
-                }</Link></Button>
-            </div>
-        </div>
-    )
+    setToken(storedToken);
+  }, []); // Run this effect once on component mount
+
+  return (
+    <div className="navbar-container">
+      <div className="logo-container">
+        <Link to='/'>
+          <h1>Zipifly</h1>
+        </Link>
+      </div>
+      <div className="menu-container">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/service'>Services</Link></li>
+          <li><Link to='/offers'>Offers</Link></li>
+        </ul>
+      </div>
+      <div className="button-container">
+        <Button variant="outlined" color='success'>
+          <Link to='/welcome'>
+            {token ? `Dashboard` : `Log in`}
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
